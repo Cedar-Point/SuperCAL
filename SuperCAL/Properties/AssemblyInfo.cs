66c63d0366bf2055e2607d493b6135c1d25eba33
Dylan K. Bickerstaff <dylan@belowaverage.org>
2019-02-12 13:18:06 -0500

    Fixed Elevation Issue.

--- SuperCAL/Properties/AssemblyInfo.cs
+++ SuperCAL/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SuperCAL")]
 [assembly: AssemblyDescription("Super CAL for Simphony!")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyCompany("Cedar Point")]
+[assembly: AssemblyCompany("Dylan Bickerstaff")]
 [assembly: AssemblyProduct("SuperCAL")]
 [assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("Dylan Bickerstaff")]
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.3.2")]
-[assembly: AssemblyFileVersion("1.1.3.2")]
+[assembly: AssemblyVersion("1.1.3.3")]
+[assembly: AssemblyFileVersion("1.1.3.3")]
 [assembly: NeutralResourcesLanguage("en")]
 
