bc1734f5660421822a42c2fcaff6ff581801fca7
Dylan Bickerstaff <dylan@belowaverage.org>
2020-03-06 08:37:52 -0500

    1.2.3.0

--- SuperCAL/Properties/AssemblyInfo.cs
+++ SuperCAL/Properties/AssemblyInfo.cs
@@ -2,37 +2,16 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("SuperCAL")]
 [assembly: AssemblyDescription("Super CAL for Simphony!")]
 [assembly: AssemblyConfiguration("Release")]
 [assembly: AssemblyCompany("Dylan Bickerstaff")]
 [assembly: AssemblyProduct("SuperCAL")]
-[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyCopyright("Copyright © 2020"]
 [assembly: AssemblyTrademark("Dylan Bickerstaff")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(true)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("12ea0caa-7402-4b6b-b75d-466f788900e2")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.2.3.0")]
 [assembly: AssemblyFileVersion("1.2.3.0")]
 [assembly: NeutralResourcesLanguage("en")]
-
